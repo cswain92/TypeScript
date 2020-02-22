2a1dae7e720a48ed80f90db47a4e4b5e767fb773
cswain92 <57902463+cswain92@users.noreply.github.com>
2020-02-22 19:52:48 +0530

    Create contract.ts
    /*The .js file of the above */

---
+++ contract.ts
@@ -0,0 +1,11 @@
+/*A contract can contain only declaration of properties and signature of methods. It can't contain any initialization , rendering and defination*/
+interface Iproduct{
+    Id:number;
+    Name:string;
+    Price:number;
+}
+let product:Iproduct= {
+ Id:1,
+ Name:"Samsung Tv",
+ Price:7666.98,
+}
